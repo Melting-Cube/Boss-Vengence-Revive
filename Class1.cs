9952c4a2d7809035c0ca58202dab2c22d8495e49
Melting-Cube <blayton784@gmail.com>
2021-01-02 19:21:41 -0700

    version 1.1.0
    Added config capability and also lets the user choose what type of boss they want to respawn after.

--- Class1.cs
+++ Class1.cs
@@ -1,33 +1,123 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using RoR2;
 using UnityEngine.Networking;
-
 
 
 namespace BossVengenceRev
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.Melting-Cube.BossVengenceRevive", "Boss and Vengence Revive", "1.0.2")]
+    [BepInPlugin("com.Melting-Cube.BossVengenceRevive", "Boss and Vengence Revive", "1.1.0")]
     public class BossVengenceRevive : BaseUnityPlugin
     {
+        //add config entries
+        public static ConfigEntry<bool> Enabled { get; set; }
+        public static ConfigEntry<bool> Doppelganger { get; set; }
+        public static ConfigEntry<bool> TeleporterBoss { get; set; }
+        public static ConfigEntry<bool> MiscBoss { get; set; }
+
+    //respawn method to call
+    public void RespawnChar()
+        {
+            //see if they are playing with others
+            bool solo = RoR2.RoR2Application.isInSinglePlayer || !NetworkServer.active;
+            if (!solo)
+            {
+                //loop through every player and res the ones that are dead
+                foreach (RoR2.PlayerCharacterMasterController playerCharacterMasterController
+                         in RoR2.PlayerCharacterMasterController.instances)
+                {
+                    bool playerConnected = playerCharacterMasterController.isConnected;
+                    bool isDead = !playerCharacterMasterController.master.GetBody()
+                                || playerCharacterMasterController.master.IsDeadAndOutOfLivesServer()
+                                || !playerCharacterMasterController.master.GetBody().healthComponent.alive;
+                    if (playerConnected && isDead)
+                        playerCharacterMasterController.master.RespawnExtraLife();
+                }
+            }
+            return;
+        }
+
         public void Awake()
         {
-            Chat.AddMessage("Loaded BossVengenceRevive");
-            On.RoR2.BossGroup.OnDefeatedServer += (orig, self) =>
+            //make config entries
+            Enabled = Config.Bind<bool>(
+                "Mod Status",
+                "Enabled",
+                true,
+                "Turn the mod on or off."
+                );
+            TeleporterBoss = Config.Bind<bool>(
+                "Boss Types",
+                "Teleporter Boss",
+                true,
+                "Revive dead players after you defeat a teleporter boss."
+                );
+            Doppelganger = Config.Bind<bool>(
+                "Boss Types",
+                "Doppelganger",
+                true,
+                "Revive dead players after you defeat a Doppelganger." +
+                "\nDoppelgangers are enabled by the Vengence Artifact"
+                );
+            MiscBoss = Config.Bind<bool>(
+                "Boss Types",
+                "Special Bosses",
+                false,
+                "Revive dead players after you defeat a Special Boss." +
+                "\nSpecial bosses are ones such as Aurelionite or Alloy Worship Unit."
+                );
+
+            //On.RoR2.Networking.GameNetworkManager.OnClientConnect += (self, user, t) => { };
+            Logger.LogInfo("Loaded BossVengenceRevive");
+
+            //is mod enabled
+            if (!Enabled.Value)
+                return;
+
+            //everytime a character is hit
+            On.RoR2.CharacterBody.OnTakeDamageServer += (orig, self, damageReport) =>
             {
-                orig(self);
-                //see if they are playing with others
-                bool solo = RoR2.RoR2Application.isInSinglePlayer || !NetworkServer.active;
-                if (!solo)
+                //call original method
+                orig(self, damageReport);
+
+                //exit if the entity is alive
+                if (self.healthComponent.alive)
+                return;
+
+                //see if there is a boss and if character has model
+                if (BossGroup.FindBossGroup(self) == null
+                    || self.GetComponent<ModelLocator>()?.modelTransform?.GetComponent<CharacterModel>() == null)
+                    return;
+
+                //is character last member
+                if (BossGroup.FindBossGroup(self).combatSquad.memberCount <= 1)
                 {
-                    foreach (RoR2.PlayerCharacterMasterController playerCharacterMasterController in RoR2.PlayerCharacterMasterController.instances)
+                    //is defeated bossgroup doppelganger
+                    if (self.GetComponent<ModelLocator>()?.modelTransform?.GetComponent<CharacterModel>().isDoppelganger == true
+                        && Doppelganger.Value)
                     {
-                        bool playerConnected = playerCharacterMasterController.isConnected;
-                        bool isDead = !playerCharacterMasterController.master.GetBody() || playerCharacterMasterController.master.IsDeadAndOutOfLivesServer() || !playerCharacterMasterController.master.GetBody().healthComponent.alive;
-                        if (playerConnected && isDead)
-                        {
-                            playerCharacterMasterController.master.RespawnExtraLife();
-                        }
+                        RespawnChar();
+                        Logger.LogInfo("Doppelganger killed, revived dead players");
+                        return;
+                    }
+
+                    //is defeated bossgroup a teleporter boss
+                    if (BossGroup.FindBossGroup(self).GetComponent<TeleporterInteraction>()
+                        && TeleporterBoss.Value)
+                    {
+                        RespawnChar();
+                        Logger.LogInfo("Teleporter Boss killed, revived dead players");
+                        return;
+                    }
+
+                    //is defeated boss even a boss (misc.)
+                    if (self.isBoss
+                        && MiscBoss.Value)
+                    {
+                        RespawnChar();
+                        Logger.LogInfo("Special Boss killed, revived dead players");
+                        return;
                     }
                 }
             };
